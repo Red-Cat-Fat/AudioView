40d0a3fd5e28c9622514d995c57fdba7be435838
Andrey Boronnikov <a.boronnikov@corp.mail.ru>
2020-04-01 09:47:15 +0300

    Контроллер с анимацией персонажа

---
+++ Assets/AnimController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+	public Animator MyAnimator;
+	private int _speed = 5;
+    public void Update()
+    {
+	    if (Input.GetMouseButton(0))
+	    {
+			Debug.Log("Click");
+			//MyAnimator.SetTrigger("SpaceClick");
+			_speed++;
+	    }
+
+	    if (Input.GetMouseButton(1))
+	    {
+		    _speed--;
+	    }
+
+		MyAnimator?.SetInteger("SpeedMove", _speed);
+    }
+}
